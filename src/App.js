9bf385f44f678c8ee315c770333386f1c9286ed2
ahmfahim <fahimmorshed2019@gmail.com>
2021-08-16 01:10:24 +0600

    color generator

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import SingleColor from './SingleColor'
+
+import Values from 'values.js'
+
+function App() {
+
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values('#6600ff').all(1));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try{
+      let colors = new Values(color, setColor).all(10);
+      setList(colors);
+    }catch(e){
+      setError(true);
+      console.log(e);
+    }
+
+  }
+  return (
+    <>
+      <section className="container">
+        <h3>color generators- <small>by fahim</small> </h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+            className={`${error?'error':null}`}
+          />
+          <button className="btn" type="submit">submit</button>
+        </form>
+      </section>
+      <section className="colors">
+        {
+          list.map((color,index)=>{
+            return <SingleColor key={index} {...color} index = {index} hexColor={color.hex}/>
+          })
+        }
+      </section>
+    </>
+  )
+}
+
+export default App;
